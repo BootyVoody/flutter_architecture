2dd4bf6bde6e2f1b5260a106404f33924b321987
Egor Poletavkin <egor.poletavkin@cosysoft.ru>
2022-01-19 14:03:23 +0400

    Created Codegen Module

---
+++ lib/env_variables.g.dart
@@ -0,0 +1,45 @@
+// DO NOT EDIT. This is code generated via code generator
+
+class EnvironmentVariables {
+  String get shell => '/bin/zsh';
+
+  String get session_manager =>
+      'local/dsk-i5-7500-mjr-klmn:@/tmp/.ICE-unix/1092,unix/dsk-i5-7500-mjr-klmn:/tmp/.ICE-unix/1092';
+
+  String get glibcxx_force_new => '1';
+
+  String get xdg_config_dirs => '/home/cosysoft/.config/kdedefaults:/etc/xdg';
+
+  String get less => '-R';
+
+  String get xdg_session_path => '/org/freedesktop/DisplayManager/Session1';
+
+  String get nvm_inc => '/usr/share/nvm/versions/node/v16.11.0/include/node';
+
+  String get terminal_emulator => 'JetBrains-JediTerm';
+
+  String get language => 'en_US:ru';
+
+  String get d_disable_rt_screen_scale => '1';
+
+  String get _p9k_tty => '/dev/pts/1';
+
+  String get less_termcap_se => '[0m';
+
+  String get less_termcap_so => '[01;47;34m';
+
+  String get lc_address => 'ru_RU.UTF-8';
+
+  String get lc_name => 'ru_RU.UTF-8';
+
+  String get p9k_tty => 'old';
+
+  String get term_session_id => 'ccaed4e0-8a81-4866-8673-66220e0e664a';
+
+  String get __intellij_command_histfile__ =>
+      '/home/cosysoft/.cache/JetBrains/IdeaIC2021.3/terminal/history/flutter_architecture-history1';
+
+  String get homebrew_prefix => '/home/linuxbrew/.linuxbrew';
+
+  String get desktop_session => 'plasma';
+}
---
+++ packages/codegen_module/bin/generate.dart
@@ -0,0 +1,22 @@
+library codegen_module;
+
+import 'dart:io';
+
+import 'src/generator.dart';
+
+Future<void> main(List<String> args) async {
+  late int varNumber;
+
+  if (args.isNotEmpty) {
+    try {
+      varNumber = int.parse(args[0]);
+    } on FormatException {
+      print('Parametr should contain number');
+      exit(0);
+    }
+  } else {
+    varNumber = 10;
+  }
+
+  Generator().run(varNumber: varNumber);
+}
